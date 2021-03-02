faee28145fbcc99746c5094325e9aa2f69dc186f
Aleem Urrehman <33265389+khawajaaleem@users.noreply.github.com>
2021-03-02 19:19:19 +0000

    Application under test page objects

---
+++ src/test/java/org/sensyne/pages/HospitalDetailsPage.java
@@ -0,0 +1,50 @@
+package org.sensyne.pages;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class HospitalDetailsPage {
+
+    private AndroidDriver driver;
+    private WebDriverWait wait;
+
+    public HospitalDetailsPage(AndroidDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+        wait = new WebDriverWait(driver, 30);
+    }
+
+    @AndroidFindBy(className = "android.widget.TextView")
+    private List<MobileElement> details;
+
+    public String getHospitalName() {
+        return getSplitText(details.get(1).getText());
+    }
+
+    public String getType() {
+        return getSplitText(details.get(2).getText());
+    }
+
+    public String getSector() {
+        return getSplitText(details.get(3).getText());
+    }
+
+    public String getPhone() {
+        return getSplitText(details.get(details.size() - 1).getText());
+    }
+
+
+    private String getSplitText(String text) {
+        return text.split(":")[1].trim();
+    }
+
+
+}
+
---
+++ src/test/java/org/sensyne/pages/SearchPage.java
@@ -0,0 +1,55 @@
+package org.sensyne.pages;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AndroidBy;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SearchPage {
+
+    private AndroidDriver driver;
+    private WebDriverWait wait;
+
+    public SearchPage(AndroidDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+        wait = new WebDriverWait(driver, 30);
+    }
+
+    @AndroidFindBy(accessibility = "Search")
+    private MobileElement iconSearch;
+
+    @AndroidFindBy(id = "com.sensynehealth.hospitals:id/search_src_text")
+    private MobileElement txtSearch;
+
+    @AndroidFindBy(xpath = "//*[@resource-id='com.sensynehealth.hospitals:id/hospitalName']")
+    private List<MobileElement> listHospitals;
+
+    public void waitForAppLaunch(){
+        wait.until(ExpectedConditions.visibilityOf(iconSearch));
+    }
+
+    public void searchFor(String searchText){
+        iconSearch.click();
+        txtSearch.sendKeys(searchText);
+    }
+
+    public List<String> hospitalResults(){
+        return listHospitals.stream().map(RemoteWebElement::getText).collect(Collectors.toList());
+    }
+
+    public void selectHospital(String name){
+        listHospitals.stream().filter(ele -> ele.getText().equalsIgnoreCase(name)).findFirst().get().click();
+    }
+
+}
